4c0f47d9078166123865917eb9c9061782d2b283
eugene <eugsokolov@gmail.com>
2015-09-11 13:36:07 -0400

    optimal Ridge regression w/o feature selection 68.5%

--- kaggle/linear_reg/runRidge.py
+++ kaggle/linear_reg/runRidge.py
@@ -3,5 +3,5 @@ import sys
 sys.path.append('../template_script')
 import templateScript
 
-ridge = linear_model.Ridge(alpha=0.1)
+ridge = linear_model.Ridge(alpha=0.000001)
 templateScript.doScript("linear_reg",ridge)

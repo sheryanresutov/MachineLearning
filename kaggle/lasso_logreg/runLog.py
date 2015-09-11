f2f9c8fd60e0c246a79fe0d645db67c979a33594
eugene <eugsokolov@gmail.com>
2015-09-11 11:10:18 -0400

    updated lasso/log reg

---
+++ kaggle/lasso_logreg/runLog.py
@@ -0,0 +1,7 @@
+from sklearn import linear_model
+import sys
+sys.path.append('../template_script')
+import templateScript
+
+log = linear_model.LogisticRegression(C=1e5)
+templateScript.doScript("lasso_logreg",log)

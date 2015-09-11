d2e3ec2f279457e3a59f4d0c2231cc7d7599f183
eugene <eugsokolov@gmail.com>
2015-09-11 13:26:12 -0400

    update

--- kaggle/linear_reg/runmultiNB.py
+++
@@ -1,7 +0,0 @@
-from sklearn import naive_bayes
-import sys
-sys.path.append('../template_script')
-import templateScript
-
-nb = naive_bayes.MultinomialNB(alpha=1.0)
-templateScript.doScript("linear_reg",nb)

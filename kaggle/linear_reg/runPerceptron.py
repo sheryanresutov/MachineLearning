103f49e5478278694139b227a44b927c406ca574
eugene <eugsokolov@gmail.com>
2015-09-11 13:06:59 -0400

    optimal Perceptron 79.59%

--- kaggle/linear_reg/runPerceptron.py
+++ kaggle/linear_reg/runPerceptron.py
@@ -3,5 +3,5 @@ import sys
 sys.path.append('../template_script')
 import templateScript
 
-p = linear_model.Perceptron(alpha=0.1)
+p = linear_model.Perceptron(alpha=1e5)
 templateScript.doScript("linear_reg",p)

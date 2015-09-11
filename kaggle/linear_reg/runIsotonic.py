090aec1da8fd412d12c2dec425fec9c005cab63a
eugene <eugsokolov@gmail.com>
2015-09-11 13:44:21 -0400

    update

--- kaggle/linear_reg/runIsotonic.py
+++
@@ -1,7 +0,0 @@
-from sklearn import isotonic
-import sys
-sys.path.append('../template_script')
-import templateScript
-
-ir = isotonic.IsotonicRegression()
-templateScript.doScript("linear_reg",ir)

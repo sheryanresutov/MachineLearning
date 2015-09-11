95bb4477d67f7552f78ee87281b3d5d91c9700bb
eugene <eugsokolov@gmail.com>
2015-09-11 13:42:04 -0400

    update

--- kaggle/linear_reg/runTheilSenReg.py
+++
@@ -1,7 +0,0 @@
-from sklearn import linear_model
-import sys
-sys.path.append('../template_script')
-import templateScript
-
-tsr = linear_model.TheilSenRegressor()
-templateScript.doScript("linear_reg",tsr)

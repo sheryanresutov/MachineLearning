7d99f76e584f5556f88126541bb2148cc96e2374
Sheryan Resutov <sheryan.resutov@yodle.com>
2015-09-14 22:50:11 -0400

    even more optimal parameters C and gamma

--- kaggle/svm/runSVM.py
+++ kaggle/svm/runSVM.py
@@ -3,5 +3,5 @@ import sys
 sys.path.append('/home/sheryan/Documents/EEclasses/Machine Learning/MachineLearning/kaggle/template_script')
 import templateScript
 
-svm = clf=SVC(C=16, kernel='rbf', gamma=0.02)
+svm = clf=SVC(C=19, kernel='rbf', gamma=0.019)
 templateScript.doScript("svm",svm)

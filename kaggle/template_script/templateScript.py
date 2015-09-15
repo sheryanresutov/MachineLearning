118cca323c86f7d15a85d52a5891718dd8fe3f17
Sheryan Resutov <sheryan.resutov@yodle.com>
2015-09-14 20:58:30 -0400

    modified template script to use 100% data when predicting test labels

--- kaggle/template_script/templateScript.py
+++ kaggle/template_script/templateScript.py
@@ -5,6 +5,8 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import datetime
+from sklearn.svm import SVR
+from sklearn.feature_selection import RFECV
 
 trainFile = "../training_data/train_data.csv"
 testFile = "../training_data/test_x_data.csv"
@@ -17,6 +19,8 @@ b = [i[561] for i in X]
 b = np.asarray(b,dtype=np.double)
 cv = cross_validation.KFold(len(X), n_folds=5)
 
+#XX=X
+#RFECV(SVR(kernel="linear"), step=1, cv=5).fit_transform(np.delete(XX,561,1), b)
 
 def doScript(submissionFolder,model):
     count=0
@@ -26,7 +30,7 @@ def doScript(submissionFolder,model):
         label_set= b[testcv]
         train_data = np.delete(train_set,561,1)
         target_data = [data[561] for data in train_set]
-            
+        
         model.fit(train_data, target_data)
         predicted = model.predict(test_set)
         for i in range(len(label_set)):
@@ -40,6 +44,7 @@ def doScript(submissionFolder,model):
         test = pd.DataFrame(pd.read_csv(file))
     X_test=test.values.tolist()
     X_test=np.asarray(X_test, dtype=np.double)
+    model.fit(np.delete(X,561, 1), [data[561] for data in X])
     test_predicted = model.predict(X_test)
     np.savetxt("../"+submissionFolder+"/submissions/"+str(model)+"_"+accuracy+"_"+str(datetime.datetime.now().time())+".csv",test_predicted)
 
